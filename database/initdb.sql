c812ba4f7cc9bcf611044589db040e8f601ab5ec
Fabio <flbertagna@gmail.com>
2021-01-11 13:02:50 +0100

    Update model and make postgresql persistence dir

--- database/initdb.sql
+++ database/initdb.sql
@@ -4,8 +4,8 @@ CREATE TABLE "adressen" (
   "plz" int,
   "adresse_1" varchar,
   "adresse_2" varchar,
-  "tel_G" varchar,
-  "tel_M" varchar,
+  "tel_g" varchar,
+  "tel_m" varchar,
   "email_1" varchar,
   "email_2" varchar
 );
@@ -24,13 +24,13 @@ CREATE TABLE "schulen" (
   "fk_aid" int
 );
 
-CREATE TABLE "firma" (
+CREATE TABLE "firmen" (
   "fid" SERIAL PRIMARY KEY,
   "name" varchar,
   "fk_aid" int
 );
 
-CREATE TABLE "lehrkraft" (
+CREATE TABLE "lehrkraefte" (
   "lkid" SERIAL PRIMARY KEY,
   "fk_pid" int
 );
@@ -42,7 +42,7 @@ CREATE TABLE "kalssen" (
   "fk_lkid" int
 );
 
-CREATE TABLE "abv" (
+CREATE TABLE "abvs" (
   "abvid" SERIAL PRIMARY KEY,
   "fk_pid" int,
   "fk_fid" int
@@ -55,38 +55,52 @@ CREATE TABLE "schueler" (
   "fk_kid" int
 );
 
-CREATE TABLE "beurteilung" (
+CREATE TABLE "beurteilungen" (
   "bid" SERIAL PRIMARY KEY,
   "fk_sid" int,
+  "fk_mid" int,
   "note" int2 NOT NULL,
   "gewichtung" float4 NOT NULL DEFAULT 1,
   "bemerkung" text
+);
+
+CREATE TABLE "module" (
+  "mid" SERIAL PRIMARY KEY,
+  "name" varchar,
+  "fk_suid" int,
+  "fk_lkid" int
 );
 
 ALTER TABLE "personen" ADD FOREIGN KEY ("fk_aid") REFERENCES "adressen" ("aid");
 
 ALTER TABLE "schulen" ADD FOREIGN KEY ("fk_aid") REFERENCES "adressen" ("aid");
 
-ALTER TABLE "firma" ADD FOREIGN KEY ("fk_aid") REFERENCES "adressen" ("aid");
+ALTER TABLE "firmen" ADD FOREIGN KEY ("fk_aid") REFERENCES "adressen" ("aid");
 
-ALTER TABLE "lehrkraft" ADD FOREIGN KEY ("fk_pid") REFERENCES "personen" ("pid");
+ALTER TABLE "lehrkraefte" ADD FOREIGN KEY ("fk_pid") REFERENCES "personen" ("pid");
 
 ALTER TABLE "kalssen" ADD FOREIGN KEY ("fk_suid") REFERENCES "schulen" ("suid");
 
-ALTER TABLE "kalssen" ADD FOREIGN KEY ("fk_lkid") REFERENCES "lehrkraft" ("lkid");
+ALTER TABLE "kalssen" ADD FOREIGN KEY ("fk_lkid") REFERENCES "lehrkraefte" ("lkid");
 
-ALTER TABLE "abv" ADD FOREIGN KEY ("fk_pid") REFERENCES "personen" ("pid");
+ALTER TABLE "abvs" ADD FOREIGN KEY ("fk_pid") REFERENCES "personen" ("pid");
 
-ALTER TABLE "abv" ADD FOREIGN KEY ("fk_fid") REFERENCES "firma" ("fid");
+ALTER TABLE "abvs" ADD FOREIGN KEY ("fk_fid") REFERENCES "firmen" ("fid");
 
 ALTER TABLE "schueler" ADD FOREIGN KEY ("fk_pid") REFERENCES "personen" ("pid");
 
-ALTER TABLE "schueler" ADD FOREIGN KEY ("fk_fid") REFERENCES "firma" ("fid");
+ALTER TABLE "schueler" ADD FOREIGN KEY ("fk_fid") REFERENCES "firmen" ("fid");
 
 ALTER TABLE "schueler" ADD FOREIGN KEY ("fk_kid") REFERENCES "kalssen" ("kid");
 
-ALTER TABLE "beurteilung" ADD FOREIGN KEY ("fk_sid") REFERENCES "schueler" ("sid");
+ALTER TABLE "beurteilungen" ADD FOREIGN KEY ("fk_sid") REFERENCES "schueler" ("sid");
 
-COMMENT ON COLUMN "beurteilung"."note" IS 'Note von 1 bis 6';
+ALTER TABLE "beurteilungen" ADD FOREIGN KEY ("fk_mid") REFERENCES "module" ("mid");
 
-COMMENT ON COLUMN "beurteilung"."gewichtung" IS 'Gewichtung von 0 bis 1';
+ALTER TABLE "module" ADD FOREIGN KEY ("fk_suid") REFERENCES "schulen" ("suid");
+
+ALTER TABLE "module" ADD FOREIGN KEY ("fk_lkid") REFERENCES "lehrkraefte" ("lkid");
+
+COMMENT ON COLUMN "beurteilungen"."note" IS 'Note von 1 bis 6';
+
+COMMENT ON COLUMN "beurteilungen"."gewichtung" IS 'Gewichtung von 0 bis 1';
